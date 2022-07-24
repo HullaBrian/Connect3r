ef44c2b2af56a01bf598109be0b65d02add90352
hullabrian <Beierlejc@gmail.com>
2022-07-23 21:17:53 -0500

    Fixed issues with build-container.sh

--- build-container.sh
+++ build-container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
+  then echo "Please run with super-user privileges"
   exit
 fi
 
@@ -14,10 +14,11 @@ fi
 
 echo "Deleting any existing docker container..."
 sudo docker rm -f connect3r_ssh
+sudo docker rmi -f tagged_connect3r_ssh
 
 echo "Constructing docker container"
 sudo docker build -t tagged_connect3r_ssh .
-docker run -d -P -p 49158:22 --name connect3r_ssh tagged_connect3r_ssh
+docker run -d -P -p 12345:22 --name connect3r_ssh tagged_connect3r_ssh
 
 IP=$(sudo docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' connect3r_ssh)
 echo "Test SSH Server given ip: ${IP}"
@@ -29,4 +30,4 @@ echo "${PORTS}"
 echo "Exporting environment variables to .env file"
 USERNAME="ssh_user"
 PASSWORD="password"
-printf "DOCKER_IP=%s\nDOCKER_USERNAME=%s\nDOCKER_PASSWORD=%s\nDOCKER_PORT=49158" "${IP}" "${USERNAME}" "${PASSWORD}" > .env
+printf "DOCKER_IP=%s\nDOCKER_USERNAME=%s\nDOCKER_PASSWORD=%s\nDOCKER_PORT=12345" "${IP}" "${USERNAME}" "${PASSWORD}" > .env
