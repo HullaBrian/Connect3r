13ae8eb68c2a73be6598fe07f36708b0ca16807e
b327908 <beierle.jonathan@heb.com>
2022-07-26 17:03:13 -0500

    Worked on functionality

--- connect3r/objects/SSHClient.py
+++ connect3r/objects/SSHClient.py
@@ -1,4 +1,5 @@
-from connect3r.objects.data_classes import IP as oIP
+from connect3r.objects.data_classes import IP
+from connect3r.objects.data_classes import KEX_PACKET
 from connect3r.objects.data_classes import Host
 from connect3r.objects.data_classes import Server
 
@@ -11,17 +12,10 @@ from random import randint
 
 from loguru import logger
 
-from scapy.layers.inet import IP
-from scapy.layers.inet import TCP
-from scapy.all import TCPSession
-from scapy.all import sr1
-from scapy.all import Packet
-from scapy.all import sr
-
 
 class SSHClient(socket.socket):
     def __init__(self,
-                 ip: oIP | str,
+                 ip: IP | str,
                  username: str = "",
                  password: str = "",
                  internet_address_family: socket = socket.AF_INET,
@@ -31,7 +25,7 @@ class SSHClient(socket.socket):
                  ):
         socket.socket.__init__(self, internet_address_family, socket_type, *args, **kwargs)
 
-        source_ip: oIP = oIP(socket.gethostbyname(socket.gethostname()))
+        source_ip: IP = IP(socket.gethostbyname(socket.gethostname()))
         ssh_version: str = check_output(
             ["ssh", "-V"]
             , stderr=STDOUT) \
@@ -39,9 +33,40 @@ class SSHClient(socket.socket):
             .replace("\r", "") \
             .replace("\n", "")
         local_port: int = randint(1024, 65535)
-        self.host: Host = Host(source_ip, "", "", local_port, username, password, ssh_version)
+        supported_kex_algorithms = [
+            "curve25519-sha256",
+            "curve25519-sha256@libssh.org",
+            "diffie-hellman-group-exchange-sha256"
+        ]
+        supported_encryption_algorithms = [
+            "aes256-ctr"
+        ]
+        supported_mac_algorithms = [
+            "hmac-sha2-512"
+        ]
+        supported_compression_algorithms = [
+            "none",
+            "zlib@openssh.com",
+            "zlib"
+        ]
+        self.host: Host = Host(
+            source_ip
+            , ""
+            , ""
+            , local_port
+            , username
+            , password
+            , ssh_version
+            , supported_kex_algorithms
+            , supported_encryption_algorithms
+            , supported_mac_algorithms
+            , supported_compression_algorithms
+        )
 
-        self.server: Server = Server(ip, "", "", 22)
+        supported_server_host_key_algorithms = [
+            "rsa-sha2-512"
+        ]
+        self.server: Server = Server(ip, "", "", 22, "", supported_server_host_key_algorithms)
 
         if self._has_ssh():
             pass
@@ -74,11 +99,76 @@ class SSHClient(socket.socket):
         return False
 
     def connect(self, **kwargs):
-        # Synchronize
-        ip = IP(src=self.host.ip, dst=self.server.ip)
-        syn = TCP(sport=self.host.port, dport=self.server.port, flags='S', seq=1000)
-        synack: Packet | None = sr1(ip / syn)
+        logger.info("verifying that device has ssh")
+        logger.debug("Source ip: " + str(self.host.ip))
+        logger.debug("Source port: " + str(self.host.port))
+        logger.debug("Destination ip: " + str(self.server.ip))
+        logger.debug("Destination port: " + str(self.server.port))
+        try:
+            super().connect((str(self.server.ip), self.server.port))
 
-        # ack
-        ack = TCP(sport=self.host.port, dport=self.server.port, flags='A', seq=synack.ack, ack=synack.seq + 1)
-        response = sr(syn / ack)
+            # Send SSH version
+            super().send(b"SSH-2.0-" + bytes(self.host.ssh_version.split("p1")[0]) + bytearray.fromhex("0d0a"))
+            # Receive SSH Version
+            response = super().recv(1024)
+
+            # Initiate key exchange
+            self._kex()
+
+            super().close()
+        except OSError as exception:
+            logger.critical("connection refused. Is the device on? Error: " + str(exception))
+        except Exception as exception:
+            logger.critical("Error during connection: " + str(exception))
+
+    def _kex(self):
+        kex_packet = KEX_PACKET(
+            packet_length=0,
+            padding_length=0,
+            message_code_kex_init=0,
+            cookie="",
+            kex_algorithms_length=0,
+            kex_algorithms_string_truncated="",
+            server_host_key_algorithms_length=0,
+            server_host_key_algorithms_string="",
+            encryption_algorithms_client_to_server_length=0,
+            encryption_algorithms_client_to_server_string="",
+            encryption_algorithms_server_to_client_length=0,
+            encryption_algorithms_server_to_client_string="",
+            mac_algorithms_client_to_server_length=0,
+            mac_algorithms_client_to_server_string="",
+            mac_algorithms_server_to_client_length=0,
+            mac_algorithms_server_to_client_string="",
+            compression_algorithms_client_to_server_length=0,
+            compression_algorithms_client_to_server_string="",
+            compression_algorithms_server_to_client_length=0,
+            compression_algorithms_server_to_client_string="",
+            languages_client_to_server_length=0,
+            languages_client_to_server_string="",
+            languages_server_to_client_length=0,
+            languages_server_to_client_string="",
+            first_kex_packet_follows=0,
+            reserved="",
+            padding_string=""
+        )
+        packet: bytes = bytearray.fromhex("00000105")  # 4
+        packet += bytearray.fromhex("05")  # 1
+        packet += bytearray.fromhex("14")  # 1
+        packet += bytearray.fromhex("9388d38ee73c2a5dc6808fd2dcd2ebd7")  # 16
+        packet += bytearray.fromhex("00000053")  # 4  20
+        packet += b"curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256"  # 17
+        packet += bytearray.fromhex("0000000C")  # 4
+        packet += b"rsa-sha2-512"  # 12
+        packet += bytearray.fromhex("0000000A")  # 4
+        packet += b"aes256-ctr"  # 10
+        packet += bytearray.fromhex("0000000A")  # 4
+        packet += b"aes256-ctr"  # 10
+        packet += bytearray.fromhex("0000000D")  # 4
+        packet += b"hmac-sha2-512"  # 13
+        packet += bytearray.fromhex("0000000D")  # 4
+        packet += b"hmac-sha2-512"  # 13
+        packet += bytearray.fromhex("0000001A")  # 4
+        packet += b"none,zlib@openssh.com,zlib"  # 26
+        packet += bytearray.fromhex("0000001A")  # 4
+        packet += b"none,zlib@openssh.com,zlib"  # 26
+        packet += bytearray.fromhex("00" * 18)  # footer/padding  # 9
--- connect3r/objects/data_classes.py
+++ connect3r/objects/data_classes.py
@@ -1,4 +1,8 @@
 from dataclasses import dataclass
+import secrets
+from cryptography.hazmat.primitives import serialization as crypto_serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend as crypto_default_backend
 
 
 @dataclass
@@ -18,6 +22,10 @@ class Host:
     username: str
     password: str
     ssh_version: str
+    supported_kex_algorithms: list[str]
+    supported_encryption_algorithms: list[str]
+    supported_mac_algorithms: list[str]
+    supported_compression_algorithms: list[str]
 
 
 @dataclass
@@ -26,3 +34,73 @@ class Server:
     mac_address: str
     key_cache: str
     port: int
+    ssh_version: str
+    supported_server_host_key_algorithms: list[str]
+
+
+def _get_hex_padding(num: int) -> bytes:
+    return bytearray.fromhex(str(hex(num)).zfill(8))
+
+
+def _generate_key_pair() -> tuple[bytes, bytes]:
+    key = rsa.generate_private_key(
+        backend=crypto_default_backend(),
+        public_exponent=65537,
+        key_size=256
+    )
+
+    private_key = key.private_bytes(
+        crypto_serialization.Encoding.OpenSSH,
+        crypto_serialization.PrivateFormat.PKCS8,
+        crypto_serialization.NoEncryption()
+    )
+
+    public_key = key.public_key().public_bytes(
+        crypto_serialization.Encoding.OpenSSH,
+        crypto_serialization.PublicFormat.OpenSSH
+    )
+
+    return private_key, public_key
+
+
+@dataclass
+class KEX_PACKET:
+    packet_length: int  # 4 hex pairs: num < 4294967295
+    padding_length: int  # 1 hex pair: num < 255
+    message_code_kex_init: int  # 1 hex pair: num < 255
+    cookie: str  # 16 hex pairs
+    kex_algorithms_length: int  # (Length of ASCII text) 4 hex pairs: num < 4294967295
+    kex_algorithms_string_truncated: str  # <kex_algorithms_length> hex pairs
+    server_host_key_algorithms_length: int  # 4 hex pairs: num < 4294967295
+    server_host_key_algorithms_string: str
+    encryption_algorithms_client_to_server_length: int  # 4 hex pairs: num < 4294967295
+    encryption_algorithms_client_to_server_string: str
+    encryption_algorithms_server_to_client_length: int  # 4 hex pairs: num < 4294967295
+    encryption_algorithms_server_to_client_string: str
+    mac_algorithms_client_to_server_length: int  # 4 hex pairs: num < 4294967295
+    mac_algorithms_client_to_server_string: str
+    mac_algorithms_server_to_client_length: int  # 4 hex pairs: num < 4294967295
+    mac_algorithms_server_to_client_string: str
+    compression_algorithms_client_to_server_length: int  # 4 hex pairs: num < 4294967295
+    compression_algorithms_client_to_server_string: str
+    compression_algorithms_server_to_client_length: int  # 4 hex pairs: num < 4294967295
+    compression_algorithms_server_to_client_string: str
+    languages_client_to_server_length: int  # 4 hex pairs: num < 4294967295
+    languages_client_to_server_string: str
+    languages_server_to_client_length: int  # 4 hex pairs: num < 4294967295
+    languages_server_to_client_string: str
+    first_kex_packet_follows: int  # 1 hex pair: num < 255. Default 0
+    reserved: str  # 4 hex pairs: "00 00 00 00"
+    padding_string: str  # 5 hex pairs: "00 00 00 00 00"
+
+    def construct(self):
+        self.message_code_kex_init = bytearray.fromhex("14")
+
+    def _generate_cookie(self):
+        self.cookie = secrets.token_hex(32)
+
+    def _get_padding(self) -> int:
+        return (self.packet_length + 5) % 8
+
+
+
